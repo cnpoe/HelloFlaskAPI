e542c54650a93011e6648d506e9f1ba986558274
cnpoe <cnpoek@gmail.com>
2018-07-04 06:34:11 -0500

    requirement c complete

--- api.py
+++ api.py
@@ -1,19 +1,24 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_api import status
+import requests
+
 
 app = Flask(__name__)
 api = Api(app)
 
 
 class HelloResource(Resource):
-    def get(self):
-        return {"res": "estoy funcionando xD"}, 200
 
     def post(self):
         data = request.get_json(force=True)
+        message = ""
+        response_ = requests.get('http://192.241.215.103:8889/').json()
+        message += response_["res"] + ' '
+        response_ = requests.get('http://192.241.215.103:8888/').json()
+        message += response_["res"]
         response = {
-            'message': 'hello world!!!',
+            'message': f'{message}!!!',
             'parameters': data
         }
         return response, status.HTTP_200_OK
